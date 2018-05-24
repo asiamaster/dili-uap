d37a94e502594269a10d0cd11117eaacaa461c39
wangmi <wangmi@diligrp.com>
2018-05-24 17:36:43 +0800

    提交部分代码

---
+++ uap/src/main/java/com/dili/uap/constants/UapConstants.java
@@ -0,0 +1,16 @@
+package com.dili.uap.constants;
+
+/**
+ * 系统常量配置
+ */
+public class UapConstants {
+
+    /**
+     * 统一权限平台系统编码
+     */
+    public static final String SYSTEM_CODE = "UAP";
+//    密码错误多次后锁定用户
+    public static final String LOGIN_FAILED_TIMES = "loginFailedTimes";
+//    锁定用户恢复时长(毫秒)，默认12小时
+    public static final String RESUME_DURATION = "resumeDuration";
+}
