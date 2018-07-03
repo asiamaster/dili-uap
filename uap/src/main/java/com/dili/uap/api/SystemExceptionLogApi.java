9c23be3190875e37e5f2bc74ba970e81e30c58dc
wangmi <wangmi@diligrp.com>
2018-07-03 09:16:31 +0800

    去掉无用的import

--- uap/src/main/java/com/dili/uap/api/SystemExceptionLogApi.java
+++ uap/src/main/java/com/dili/uap/api/SystemExceptionLogApi.java
@@ -1,30 +1,17 @@
 package com.dili.uap.api;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.dili.ss.domain.BaseOutput;
-import com.dili.ss.dto.DTOUtils;
-import com.dili.uap.domain.DataAuth;
 import com.dili.uap.sdk.domain.SystemExceptionLog;
-import com.dili.uap.domain.UserDataAuth;
-import com.dili.uap.manager.DataAuthManager;
-import com.dili.uap.service.DataAuthService;
 import com.dili.uap.service.SystemExceptionLogService;
-import com.dili.uap.service.UserDataAuthService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.apache.commons.collections4.ListUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 /**
  * 系统异常日志Api
