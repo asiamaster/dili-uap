26877c52a8bc66d946012e77f7221fd8cda78caf
wangmi <wangmi@diligrp.com>
2018-07-11 14:34:39 +0800

    去掉用户部门关系模块

--- uap/src/main/java/com/dili/uap/dao/UserDepartmentMapper.java
+++
@@ -1,7 +0,0 @@
-package com.dili.uap.dao;
-
-import com.dili.ss.base.MyMapper;
-import com.dili.uap.domain.UserDepartment;
-
-public interface UserDepartmentMapper extends MyMapper<UserDepartment> {
-}
