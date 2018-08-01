53b82eb103ece3e77f2a4d50cfe1b736d116a9eb
岳洪波 <yuehongbo@diligrp.com>
2018-08-01 11:24:17 +0800

    增加getByCode方法

--- uap/src/main/java/com/dili/uap/api/SystemConfigApi.java
+++ uap/src/main/java/com/dili/uap/api/SystemConfigApi.java
@@ -1,11 +1,14 @@
 package com.dili.uap.api;
 
 import com.dili.ss.domain.BaseOutput;
+import com.dili.ss.dto.DTOUtils;
 import com.dili.uap.sdk.domain.SystemConfig;
 import com.dili.uap.service.SystemConfigService;
 import io.swagger.annotations.Api;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -32,6 +35,19 @@ public class SystemConfigApi {
 	@RequestMapping(value = "/saveOrUpdate.api", method = { RequestMethod.POST })
 	public BaseOutput saveOrUpdate(SystemConfig systemConfig) {
 		return BaseOutput.success().setData(this.systemConfigService.saveOrUpdate(systemConfig));
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/getByCode.api", method = { RequestMethod.POST })
+	public BaseOutput<SystemConfig> getByCode(@RequestBody String code) {
+		if (StringUtils.isBlank(code)){
+			return BaseOutput.success();
+		}
+		SystemConfig query = DTOUtils.newDTO(SystemConfig.class);
+		query.setCode(code);
+		return BaseOutput.success().setData(this.systemConfigService.list(query).stream().findFirst().orElseGet(() -> {
+			return null;
+		}));
 	}
 
 }
