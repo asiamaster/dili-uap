c50c1988f6822af85e375e72db78362851af7850
wangmi <wangmi@diligrp.com>
2019-10-14 17:57:53 +0800

    2.4.2 添加JVM信息打印

--- uap/src/main/java/com/dili/UapApplication.java
+++ uap/src/main/java/com/dili/UapApplication.java
@@ -36,7 +36,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @ComponentScan(basePackages = { "com.dili.ss", "com.dili.uap"})
 @RestfulScan({"com.dili.uap.rpc", "com.dili.uap.sdk.rpc"})
 @MapperScan(basePackages = {"com.dili.uap.dao", "com.dili.ss.uid.dao", "com.dili.ss.dao", "com.dili.ss.quartz.dao"})
-@DTOScan(value={"com.dili.ss", "com.dili.uap"}, file="/DTOtargetClass")
+@DTOScan(value={"com.dili.ss", "com.dili.uap"})
 /**
  * 除了内嵌容器的部署模式，Spring Boot也支持将应用部署至已有的Tomcat容器, 或JBoss, WebLogic等传统Java EE应用服务器。
  * 以Maven为例，首先需要将<packaging>从jar改成war，然后取消spring-boot-maven-plugin，然后修改Application.java
@@ -50,6 +50,14 @@ public class UapApplication extends SpringBootServletInitializer implements Comm
 
 	@Override
 	public void run(String... args) {
+//		maxMemory=Eden+Survivor+Old Gen
+//		maxMemory是拿到的程序最大可以使用的内存，
+//		我们知道 ，Survivor有两个，但只有1个会用到，另一个一直闲置。
+//		所以这个值maxMemory是去掉一个Survivor空间的值。
+		long maxMemory = Runtime.getRuntime().maxMemory();
+		long totalMemory = Runtime.getRuntime().totalMemory();
+		long freeMemory = Runtime.getRuntime().freeMemory();
+		System.out.println("maxMemory:"+maxMemory+",totalMemory:"+totalMemory+",freeMemory:"+freeMemory);
 		System.out.println("项目启动完成!");
 	}
 }
