aa2505ec933b1f4856cafda4d3c52d0ed4057e8b
jiang <jiang@DESKTOP-I2028PV>
2019-12-30 16:54:51 +0800

    增加项目数据权限获取接口，未实现方法逻辑

---
+++ uap/src/main/java/com/dili/uap/component/ProjectDataAuthSourceService.java
@@ -0,0 +1,25 @@
+package com.dili.uap.component;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.dili.uap.sdk.service.DataAuthSourceService;
+
+@Component
+public class ProjectDataAuthSourceService implements DataAuthSourceService {
+
+	@Override
+	public List listDataAuthes(String param) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Map> bindDataAuthes(String param, List<String> values) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
