c96f2cefa0ea38ac8588ef5ec193ca554384dd5d
jianglinhu <jianglinhu@diligrp.com>
2020-01-17 17:14:10 +0800

    增加根据角色id批量查询角色关联用户的接口

--- uap/src/main/java/com/dili/uap/api/RoleApi.java
+++ uap/src/main/java/com/dili/uap/api/RoleApi.java
@@ -2,10 +2,13 @@ package com.dili.uap.api;
 
 import com.dili.ss.domain.BaseOutput;
 import com.dili.uap.sdk.domain.Role;
+import com.dili.uap.sdk.domain.dto.RoleUserDto;
 import com.dili.uap.service.RoleService;
 import io.swagger.annotations.Api;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,26 +23,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/roleApi")
 public class RoleApi {
-    @Autowired
-    RoleService roleService;
+	@Autowired
+	RoleService roleService;
 
+	/**
+	 * 根据用户id或用户名查询角色
+	 * 
+	 * @param userId
+	 * @param userName
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/listByUser.api", method = { RequestMethod.GET, RequestMethod.POST })
+	public BaseOutput<List<Role>> listByUser(@RequestParam(required = false) Long userId, @RequestParam(required = false) String userName) throws Exception {
+		if (userId != null) {
+			return BaseOutput.success().setData(roleService.listByUserId(userId));
+		} else if (StringUtils.isNotBlank(userName)) {
+			return BaseOutput.success().setData(roleService.listByUserName(userName));
+		}
+		return BaseOutput.failure("参数为空");
+	}
 
-    /**
-     * 根据用户id或用户名查询角色
-     * @param userId
-     * @param userName
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping(value="/listByUser.api", method = {RequestMethod.GET, RequestMethod.POST})
-    public BaseOutput<List<Role>> listByUser(@RequestParam(required = false) Long userId, @RequestParam(required = false) String userName) throws Exception {
-        if(userId != null){
-            return BaseOutput.success().setData(roleService.listByUserId(userId));
-        }else if(StringUtils.isNotBlank(userName)){
-            return BaseOutput.success().setData(roleService.listByUserName(userName));
-        }
-        return BaseOutput.failure("参数为空");
-    }
-
-
+	@PostMapping("/listRoleUserByRoleIds.api")
+	public BaseOutput<List<RoleUserDto>> listRoleUserByRoleIds(@RequestBody List<Long> roleIds) {
+		List<RoleUserDto> list = this.roleService.listRoleUserByRoleIds(roleIds);
+		return BaseOutput.success().setData(list);
+	}
 }
