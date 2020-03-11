7183671e7bfe0f6659e14b476212bffbe7a3bfaf
jiang <jiang@DESKTOP-GJ4LO3T>
2020-03-11 14:48:21 +0800

    修改数据字典修改、新增判断逻辑

--- uap/src/main/java/com/dili/uap/service/impl/DataDictionaryServiceImpl.java
+++ uap/src/main/java/com/dili/uap/service/impl/DataDictionaryServiceImpl.java
@@ -15,69 +15,73 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * 由MyBatis Generator工具自动生成
- * This file was generated on 2018-05-21 10:39:45.
+ * 由MyBatis Generator工具自动生成 This file was generated on 2018-05-21 10:39:45.
  */
 @Service
 public class DataDictionaryServiceImpl extends BaseServiceImpl<DataDictionary, Long> implements DataDictionaryService {
-	@Autowired DataDictionaryValueMapper dataDictionaryValueMapper;
-    public DataDictionaryMapper getActualDao() {
-        return (DataDictionaryMapper)getDao();
-    }
-    @Transactional
-    @Override
-    public BaseOutput<Object> deleteAfterCheck(Long id) {
-    	DataDictionary dataDictionary=this.get(id);
-    	
-    	//根据code删除数据字典的值
-    	if(dataDictionary!=null&&dataDictionary.getCode()!=null) {
-    		DataDictionaryValue condition=DTOUtils.newInstance(DataDictionaryValue.class);
-    		condition.setDdCode(dataDictionary.getCode());
-    		int count=this.dataDictionaryValueMapper.selectCount(condition);
-    		if(count>0) {
-    			return BaseOutput.failure("当前数据字典不为空，不允许删除");	
-    		}
-    	}
-    	super.delete(id);
-    	
-    	return BaseOutput.success("删除成功");
-    }
+	@Autowired
+	DataDictionaryValueMapper dataDictionaryValueMapper;
+
+	public DataDictionaryMapper getActualDao() {
+		return (DataDictionaryMapper) getDao();
+	}
+
+	@Transactional
+	@Override
+	public BaseOutput<Object> deleteAfterCheck(Long id) {
+		DataDictionary dataDictionary = this.get(id);
+
+		// 根据code删除数据字典的值
+		if (dataDictionary != null && dataDictionary.getCode() != null) {
+			DataDictionaryValue condition = DTOUtils.newInstance(DataDictionaryValue.class);
+			condition.setDdCode(dataDictionary.getCode());
+			int count = this.dataDictionaryValueMapper.selectCount(condition);
+			if (count > 0) {
+				return BaseOutput.failure("当前数据字典不为空，不允许删除");
+			}
+		}
+		super.delete(id);
+
+		return BaseOutput.success("删除成功");
+	}
+
 	@Override
 	public BaseOutput<Object> insertAfterCheck(DataDictionary t) {
-		
-		if(StringUtils.isBlank(t.getCode())){
+
+		if (StringUtils.isBlank(t.getCode())) {
 			return BaseOutput.failure("编码不能为空");
 		}
-		if(StringUtils.isBlank(t.getSystemCode())){
+		if (StringUtils.isBlank(t.getSystemCode())) {
 			return BaseOutput.failure("系统编码不能为空");
 		}
-		DataDictionary condition=DTOUtils.newInstance(DataDictionary.class);
+		DataDictionary condition = DTOUtils.newInstance(DataDictionary.class);
 		condition.setCode(StringUtils.trim(t.getCode()));
-		condition.setSystemCode(StringUtils.trim(t.getSystemCode()));
-		int size=this.list(condition).size();
-		if(size>0) {
+//		condition.setSystemCode(StringUtils.trim(t.getSystemCode()));
+		int size = this.list(condition).size();
+		if (size > 0) {
 			return BaseOutput.failure("相同编码已经存在");
 		}
 		this.insertSelective(t);
 		return BaseOutput.success("新增成功").setData(t);
 	}
+
 	@Override
 	public BaseOutput<Object> updateAfterCheck(DataDictionary t) {
-		if(StringUtils.isBlank(t.getCode())){
+		if (StringUtils.isBlank(t.getCode())) {
 			return BaseOutput.failure("编码不能为空");
 		}
-		if(StringUtils.isBlank(t.getSystemCode())){
+		if (StringUtils.isBlank(t.getSystemCode())) {
 			return BaseOutput.failure("系统编码不能为空");
 		}
-		DataDictionary condition=DTOUtils.newInstance(DataDictionary.class);
+		DataDictionary condition = DTOUtils.newInstance(DataDictionary.class);
 		condition.setCode(StringUtils.trim(t.getCode()));
-		condition.setSystemCode(StringUtils.trim(t.getSystemCode()));
-		boolean exists=this.list(condition).stream().anyMatch((d)->!d.getId().equals(t.getId()));
-		if(exists) {
+//		condition.setSystemCode(StringUtils.trim(t.getSystemCode()));
+		boolean exists = this.list(condition).stream().anyMatch((d) -> !d.getId().equals(t.getId()));
+		if (exists) {
 			return BaseOutput.failure("相同编码已经存在");
 		}
 		this.updateSelective(t);
 		return BaseOutput.success("修改成功");
 	}
-    
+
 }
