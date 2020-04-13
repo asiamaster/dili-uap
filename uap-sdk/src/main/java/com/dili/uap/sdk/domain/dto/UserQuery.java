1bd229c52c62e869bd0c2ecc192271618c275923
岳洪波 <yuehongbo@diligrp.com>
2020-04-13 16:25:43 +0800

    用户查询对象，放到sdk中，api增加根据市场code集查询

--- uap-sdk/src/main/java/com/dili/uap/sdk/domain/dto/UserQuery.java
+++ uap-sdk/src/main/java/com/dili/uap/sdk/domain/dto/UserQuery.java
@@ -1,6 +1,7 @@
-package com.dili.uap.domain.dto;
+package com.dili.uap.sdk.domain.dto;
 
 import com.dili.ss.domain.annotation.Like;
+import com.dili.ss.domain.annotation.Operator;
 import com.dili.ss.metadata.FieldEditor;
 import com.dili.ss.metadata.annotation.EditMode;
 import com.dili.ss.metadata.annotation.FieldDef;
@@ -11,7 +12,16 @@ import javax.persistence.Column;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
+/**
+ * <B></B>
+ * <B>Copyright:本软件源代码版权归农丰时代科技有限公司及其团队所有,未经许可不得任意复制与传播.</B>
+ * <B>农丰时代科技有限公司</B>
+ *
+ * @author yuehongbo
+ * @date 2020/4/13 15:45
+ */
 @Table(name = "`user`")
 public interface UserQuery extends User {
 
@@ -25,4 +35,14 @@ public interface UserQuery extends User {
     String getUserName();
     @Override
     void setUserName(String userName);
+
+    @Operator(Operator.IN)
+    @Column(name = "`id`")
+    List<String> getIds();
+    void setIds(List<String> ids);
+
+    @Operator(Operator.IN)
+    @Column(name = "`firm_code`")
+    List<String> getFirmCodes();
+    void setFirmCodes(List<String> firmCodes);
 }
