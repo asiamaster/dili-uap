d9ea7e2dd8b375ae72cae2302d6bc6f427973352
jiang <jiang@DESKTOP-GJ4LO3T>
2020-04-28 16:22:13 +0800

    异常日志日期限制

--- uap/src/main/java/com/dili/uap/controller/SystemExceptionLogController.java
+++ uap/src/main/java/com/dili/uap/controller/SystemExceptionLogController.java
@@ -1,13 +1,18 @@
 package com.dili.uap.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.dili.ss.domain.BaseOutput;
+import com.dili.ss.util.DateUtils;
 import com.dili.uap.domain.dto.SystemExceptionLogDto;
 import com.dili.uap.sdk.domain.SystemExceptionLog;
+import com.dili.uap.sdk.session.SessionConstants;
 import com.dili.uap.service.SystemExceptionLogService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -15,101 +20,118 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
- * 由MyBatis Generator工具自动生成
- * This file was generated on 2018-05-22 15:27:04.
+ * 由MyBatis Generator工具自动生成 This file was generated on 2018-05-22 15:27:04.
  */
 @Api("/systemExceptionLog")
 @Controller
 @RequestMapping("/systemExceptionLog")
-public class SystemExceptionLogController{
-    @Autowired
-    SystemExceptionLogService systemExceptionLogService;
+public class SystemExceptionLogController {
+	@Autowired
+	SystemExceptionLogService systemExceptionLogService;
 
-    /**
-     * 跳转到SystemExceptionLog页面
-     * @param modelMap
-     * @return
-     */
-    @ApiOperation("跳转到SystemExceptionLog页面")
-    @RequestMapping(value="/index.html", method = RequestMethod.GET)
-    public String index(ModelMap modelMap) {
-        return "systemExceptionLog/index";
-    }
+	/**
+	 * 跳转到SystemExceptionLog页面
+	 * 
+	 * @param modelMap
+	 * @return
+	 */
+	@ApiOperation("跳转到SystemExceptionLog页面")
+	@RequestMapping(value = "/index.html", method = RequestMethod.GET)
+	public String index(ModelMap modelMap) {
+		return "systemExceptionLog/index";
+	}
 
-    /**
-     * 查询SystemExceptionLog
-     * @param systemExceptionLog
-     * @return
-     */
-    @ApiOperation(value="查询SystemExceptionLog", notes = "查询SystemExceptionLog，返回列表信息")
-    @ApiImplicitParams({
-		@ApiImplicitParam(name="SystemExceptionLog", paramType="form", value = "SystemExceptionLog的form信息", required = false, dataType = "string")
-	})
-    @RequestMapping(value="/list", method = {RequestMethod.GET, RequestMethod.POST})
-    public @ResponseBody List<SystemExceptionLog> list(SystemExceptionLog systemExceptionLog) {
-        return systemExceptionLogService.list(systemExceptionLog);
-    }
+	/**
+	 * 查询SystemExceptionLog
+	 * 
+	 * @param systemExceptionLog
+	 * @return
+	 */
+	@ApiOperation(value = "查询SystemExceptionLog", notes = "查询SystemExceptionLog，返回列表信息")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "SystemExceptionLog", paramType = "form", value = "SystemExceptionLog的form信息", required = false, dataType = "string") })
+	@RequestMapping(value = "/list", method = { RequestMethod.GET, RequestMethod.POST })
+	public @ResponseBody List<SystemExceptionLog> list(SystemExceptionLog systemExceptionLog) {
+		return systemExceptionLogService.list(systemExceptionLog);
+	}
 
-    /**
-     * 分页查询SystemExceptionLog
-     * @param systemExceptionLog
-     * @return
-     * @throws Exception
-     */
-    @ApiOperation(value="分页查询SystemExceptionLog", notes = "分页查询SystemExceptionLog，返回easyui分页信息")
-    @ApiImplicitParams({
-		@ApiImplicitParam(name="SystemExceptionLog", paramType="form", value = "SystemExceptionLog的form信息", required = false, dataType = "string")
-	})
-    @RequestMapping(value="/listPage.action", method = {RequestMethod.GET, RequestMethod.POST})
-    public @ResponseBody String listPage(SystemExceptionLogDto systemExceptionLog) throws Exception {
-        return systemExceptionLogService.listEasyuiPageByExample(systemExceptionLog, true).toString();
-    }
+	/**
+	 * 分页查询SystemExceptionLog
+	 * 
+	 * @param systemExceptionLog
+	 * @return
+	 * @throws Exception
+	 */
+	@ApiOperation(value = "分页查询SystemExceptionLog", notes = "分页查询SystemExceptionLog，返回easyui分页信息")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "SystemExceptionLog", paramType = "form", value = "SystemExceptionLog的form信息", required = false, dataType = "string") })
+	@RequestMapping(value = "/listPage.action", method = { RequestMethod.GET, RequestMethod.POST })
+	public @ResponseBody String listPage(SystemExceptionLogDto systemExceptionLog, HttpServletRequest req) throws Exception {
+		if (StringUtils.isNotBlank(req.getHeader(SessionConstants.SESSION_ID))) {
+			if (systemExceptionLog.getEndExceptionTime() == null) {
+				systemExceptionLog.setEndExceptionTime(new Date());
+			}
+			if (systemExceptionLog.getStartExceptionTime() == null) {
+				Calendar calendar = Calendar.getInstance();
+				calendar.add(Calendar.DAY_OF_MONTH, -31);
+				calendar.set(Calendar.HOUR_OF_DAY, 0);
+				calendar.set(Calendar.MINUTE, 0);
+				calendar.set(Calendar.MILLISECOND, 0);
+				systemExceptionLog.setStartExceptionTime(calendar.getTime());
+			} else {
+				int diff = DateUtils.differentDays(systemExceptionLog.getStartExceptionTime(), systemExceptionLog.getEndExceptionTime());
+				if (diff > 31) {
+					return JSON.toJSONString(BaseOutput.failure("只能导出31天以内的数据"));
+				}
+			}
+		}
+		return systemExceptionLogService.listEasyuiPageByExample(systemExceptionLog, true).toString();
+	}
 
-    /**
-     * 新增SystemExceptionLog
-     * @param systemExceptionLog
-     * @return
-     */
-    @ApiOperation("新增SystemExceptionLog")
-    @ApiImplicitParams({
-		@ApiImplicitParam(name="SystemExceptionLog", paramType="form", value = "SystemExceptionLog的form信息", required = true, dataType = "string")
-	})
-    @RequestMapping(value="/insert.action", method = {RequestMethod.GET, RequestMethod.POST})
-    public @ResponseBody BaseOutput insert(SystemExceptionLog systemExceptionLog) {
-        systemExceptionLogService.insertSelective(systemExceptionLog);
-        return BaseOutput.success("新增成功");
-    }
+	/**
+	 * 新增SystemExceptionLog
+	 * 
+	 * @param systemExceptionLog
+	 * @return
+	 */
+	@ApiOperation("新增SystemExceptionLog")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "SystemExceptionLog", paramType = "form", value = "SystemExceptionLog的form信息", required = true, dataType = "string") })
+	@RequestMapping(value = "/insert.action", method = { RequestMethod.GET, RequestMethod.POST })
+	public @ResponseBody BaseOutput insert(SystemExceptionLog systemExceptionLog) {
+		systemExceptionLogService.insertSelective(systemExceptionLog);
+		return BaseOutput.success("新增成功");
+	}
 
-    /**
-     * 修改SystemExceptionLog
-     * @param systemExceptionLog
-     * @return
-     */
-    @ApiOperation("修改SystemExceptionLog")
-    @ApiImplicitParams({
-		@ApiImplicitParam(name="SystemExceptionLog", paramType="form", value = "SystemExceptionLog的form信息", required = true, dataType = "string")
-	})
-    @RequestMapping(value="/update.action", method = {RequestMethod.GET, RequestMethod.POST})
-    public @ResponseBody BaseOutput update(SystemExceptionLog systemExceptionLog) {
-        systemExceptionLogService.updateSelective(systemExceptionLog);
-        return BaseOutput.success("修改成功");
-    }
+	/**
+	 * 修改SystemExceptionLog
+	 * 
+	 * @param systemExceptionLog
+	 * @return
+	 */
+	@ApiOperation("修改SystemExceptionLog")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "SystemExceptionLog", paramType = "form", value = "SystemExceptionLog的form信息", required = true, dataType = "string") })
+	@RequestMapping(value = "/update.action", method = { RequestMethod.GET, RequestMethod.POST })
+	public @ResponseBody BaseOutput update(SystemExceptionLog systemExceptionLog) {
+		systemExceptionLogService.updateSelective(systemExceptionLog);
+		return BaseOutput.success("修改成功");
+	}
 
-    /**
-     * 删除SystemExceptionLog
-     * @param id
-     * @return
-     */
-    @ApiOperation("删除SystemExceptionLog")
-    @ApiImplicitParams({
-		@ApiImplicitParam(name="id", paramType="form", value = "SystemExceptionLog的主键", required = true, dataType = "long")
-	})
-    @RequestMapping(value="/delete.action", method = {RequestMethod.GET, RequestMethod.POST})
-    public @ResponseBody BaseOutput delete(Long id) {
-        systemExceptionLogService.delete(id);
-        return BaseOutput.success("删除成功");
-    }
+	/**
+	 * 删除SystemExceptionLog
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@ApiOperation("删除SystemExceptionLog")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "id", paramType = "form", value = "SystemExceptionLog的主键", required = true, dataType = "long") })
+	@RequestMapping(value = "/delete.action", method = { RequestMethod.GET, RequestMethod.POST })
+	public @ResponseBody BaseOutput delete(Long id) {
+		systemExceptionLogService.delete(id);
+		return BaseOutput.success("删除成功");
+	}
 }
