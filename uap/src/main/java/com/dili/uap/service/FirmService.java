ce6cace7daabeb1819e22e0c6216923b9d946e1a
jiang <jiang@DESKTOP-GJ4LO3T>
2020-05-07 09:47:55 +0800

    增加注释

--- uap/src/main/java/com/dili/uap/service/FirmService.java
+++ uap/src/main/java/com/dili/uap/service/FirmService.java
@@ -5,10 +5,15 @@ import com.dili.ss.domain.BaseOutput;
 import com.dili.uap.sdk.domain.Firm;
 
 /**
- * 由MyBatis Generator工具自动生成
- * This file was generated on 2018-05-23 14:31:07.
+ * 由MyBatis Generator工具自动生成 This file was generated on 2018-05-23 14:31:07.
  */
 public interface FirmService extends BaseService<Firm, Long> {
 
+	/**
+	 * 新增市场并绑定用户数据权限
+	 * 
+	 * @param firm
+	 * @return
+	 */
 	BaseOutput<Object> insertAndBindUserDataAuth(Firm firm);
 }
