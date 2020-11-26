a2629896c202efb94fa2611a4e6177e823cb0a28
wangmi <87730066>
2020-11-26 09:57:05 +0800

    2.10.0 利刃到dili-parent 3.7.1

--- uap/src/main/java/com/dili/uap/rpc/UidRpc.java
+++ uap/src/main/java/com/dili/uap/rpc/UidRpc.java
@@ -5,6 +5,7 @@ import com.dili.uap.rpc.fallback.UidRpcFallBackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "dili-uid", contextId = "uidRpc", url="${UidRpc.url:}", fallbackFactory = UidRpcFallBackFactory.class)
 public interface UidRpc {
@@ -15,5 +16,13 @@ public interface UidRpc {
 	 */
 	@RequestMapping(value = "/api/bizNumber/sid53", method = RequestMethod.GET)
 	BaseOutput<String> getFirmSerialNumber();
+
+	/**
+	 * 获取业务编号
+	 * http://uid.diligrp.com:8282/api/bizNumber?type=hzsc_water
+	 * @return
+	 */
+	@RequestMapping(value = "/api/bizNumber", method = RequestMethod.GET)
+	BaseOutput<String> getBizNumber(@RequestParam String type);
 
 }
