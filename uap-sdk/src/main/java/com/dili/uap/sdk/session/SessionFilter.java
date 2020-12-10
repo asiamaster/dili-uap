2ce4e67a1805c01e6bed4150129ba5e22013ab7e
wangmi <87730066>
2020-12-10 11:06:46 +0800

    2.10.0 uap-sdk:去掉feign-core依赖

--- uap-sdk/src/main/java/com/dili/uap/sdk/session/SessionFilter.java
+++ uap-sdk/src/main/java/com/dili/uap/sdk/session/SessionFilter.java
@@ -55,7 +55,7 @@ public class SessionFilter implements Filter {
 	}
 
 	@Override
-	public void init(FilterConfig conf) throws ServletException {
+	public void init(FilterConfig conf) {
 		filterConfig = conf;
 	}
 
