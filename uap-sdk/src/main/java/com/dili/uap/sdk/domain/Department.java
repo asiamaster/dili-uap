41252e179269069c003e45c6e5121ea4f4f36915
jiang <jiang@DESKTOP-GJ4LO3T>
2020-12-24 11:13:32 +0800

    部门名称支持模糊查询

--- uap-sdk/src/main/java/com/dili/uap/sdk/domain/Department.java
+++ uap-sdk/src/main/java/com/dili/uap/sdk/domain/Department.java
@@ -1,5 +1,6 @@
 package com.dili.uap.sdk.domain;
 
+import com.dili.ss.domain.annotation.Like;
 import com.dili.ss.dto.IBaseDomain;
 import com.dili.ss.metadata.FieldEditor;
 import com.dili.ss.metadata.annotation.EditMode;
@@ -15,75 +16,76 @@ import java.util.Date;
  */
 @Table(name = "`department`")
 public interface Department extends IBaseDomain {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "`id`")
-    @FieldDef(label="id")
-    @EditMode(editor = FieldEditor.Number, required = true)
-    Long getId();
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "`id`")
+	@FieldDef(label = "id")
+	@EditMode(editor = FieldEditor.Number, required = true)
+	Long getId();
 
-    void setId(Long id);
+	void setId(Long id);
 
-    @Column(name = "`parent_id`")
-    @FieldDef(label="上级部门id")
-    @EditMode(editor = FieldEditor.Number, required = false)
-    Long getParentId();
+	@Column(name = "`parent_id`")
+	@FieldDef(label = "上级部门id")
+	@EditMode(editor = FieldEditor.Number, required = false)
+	Long getParentId();
 
-    void setParentId(Long parentId);
+	void setParentId(Long parentId);
 
-    @Column(name = "`firm_code`")
-    @FieldDef(label="所属市场", maxLength = 50)
-    @EditMode(editor = FieldEditor.Text, required = false)
-    String getFirmCode();
+	@Column(name = "`firm_code`")
+	@FieldDef(label = "所属市场", maxLength = 50)
+	@EditMode(editor = FieldEditor.Text, required = false)
+	String getFirmCode();
 
-    void setFirmCode(String firmCode);
+	void setFirmCode(String firmCode);
 
-    @Column(name = "`name`")
-    @FieldDef(label="名称", maxLength = 20)
-    @EditMode(editor = FieldEditor.Text, required = false)
-    String getName();
+	@Like(Like.BOTH)
+	@Column(name = "`name`")
+	@FieldDef(label = "名称", maxLength = 20)
+	@EditMode(editor = FieldEditor.Text, required = false)
+	String getName();
 
-    void setName(String name);
+	void setName(String name);
 
-    @Column(name = "`code`")
-    @FieldDef(label="编号", maxLength = 20)
-    @EditMode(editor = FieldEditor.Text, required = false)
-    String getCode();
+	@Column(name = "`code`")
+	@FieldDef(label = "编号", maxLength = 20)
+	@EditMode(editor = FieldEditor.Text, required = false)
+	String getCode();
 
-    void setCode(String code);
+	void setCode(String code);
 
-    @Column(name = "`description`")
-    @FieldDef(label="描述", maxLength = 255)
-    @EditMode(editor = FieldEditor.Text, required = false)
-    String getDescription();
+	@Column(name = "`description`")
+	@FieldDef(label = "描述", maxLength = 255)
+	@EditMode(editor = FieldEditor.Text, required = false)
+	String getDescription();
 
-    void setDescription(String description);
+	void setDescription(String description);
 
-    @Column(name = "`created_id`")
-    @FieldDef(label="创建人id")
-    @EditMode(editor = FieldEditor.Number, required = false)
-    Long getCreatedId();
+	@Column(name = "`created_id`")
+	@FieldDef(label = "创建人id")
+	@EditMode(editor = FieldEditor.Number, required = false)
+	Long getCreatedId();
 
-    void setCreatedId(Long createdId);
+	void setCreatedId(Long createdId);
 
-    @Column(name = "`created`")
-    @FieldDef(label="创建时间")
-    @EditMode(editor = FieldEditor.Datetime, required = true)
-    Date getCreated();
+	@Column(name = "`created`")
+	@FieldDef(label = "创建时间")
+	@EditMode(editor = FieldEditor.Datetime, required = true)
+	Date getCreated();
 
-    void setCreated(Date created);
+	void setCreated(Date created);
 
-    @Column(name = "`modified`")
-    @FieldDef(label="操作时间")
-    @EditMode(editor = FieldEditor.Datetime, required = true)
-    Date getModified();
+	@Column(name = "`modified`")
+	@FieldDef(label = "操作时间")
+	@EditMode(editor = FieldEditor.Datetime, required = true)
+	Date getModified();
 
-    void setModified(Date modified);
+	void setModified(Date modified);
 
-    @Column(name = "`modified_id`")
-    @FieldDef(label="修改人id")
-    @EditMode(editor = FieldEditor.Number, required = false)
-    Long getModifiedId();
+	@Column(name = "`modified_id`")
+	@FieldDef(label = "修改人id")
+	@EditMode(editor = FieldEditor.Number, required = false)
+	Long getModifiedId();
 
-    void setModifiedId(Long modifiedId);
+	void setModifiedId(Long modifiedId);
 }
