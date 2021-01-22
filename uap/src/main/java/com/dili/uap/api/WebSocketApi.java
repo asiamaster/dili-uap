5f23dc984173e38cd2aae48e80ec8ae863cc3653
wangmi <87730066>
2021-01-22 16:19:19 +0800

    3.0.0 修正MQ消息处理，保证接收端数据安全

--- uap/src/main/java/com/dili/uap/api/WebSocketApi.java
+++ uap/src/main/java/com/dili/uap/api/WebSocketApi.java
@@ -1,16 +1,15 @@
 package com.dili.uap.api;
 
 import com.alibaba.fastjson.JSON;
+import com.dili.commons.rabbitmq.RabbitMQMessageService;
 import com.dili.ss.domain.BaseOutput;
 import com.dili.uap.boot.RabbitConfiguration;
 import com.dili.uap.manager.WsSessionManager;
 import com.dili.uap.sdk.domain.dto.AnnunciateMessage;
-import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 /**
@@ -21,9 +20,8 @@ import org.springframework.web.socket.WebSocketSession;
 @RestController
 @RequestMapping("/api/ws")
 public class WebSocketApi {
-
     @Autowired
-    private AmqpTemplate amqpTemplate;
+    private RabbitMQMessageService rabbitMQMessageService;
 
     /**
      * 发送平台公告
@@ -36,12 +34,12 @@ public class WebSocketApi {
         try {
             WebSocketSession webSocketSession = WsSessionManager.get(annunciateMessage.getTargetId());
             //如果连接在本地，则直接发(多实例，用户的连接可能在其它UAP实例)
-            if(webSocketSession != null){
-                webSocketSession.sendMessage(new TextMessage(JSON.toJSONString(annunciateMessage)));
-                return BaseOutput.success("消息发送成功");
-            }
+//            if(webSocketSession != null){
+//                webSocketSession.sendMessage(new TextMessage(JSON.toJSONString(annunciateMessage)));
+//                return BaseOutput.success("消息发送成功");
+//            }
             //如果当前服务器没有连接，则发送广播消息
-            amqpTemplate.convertAndSend(RabbitConfiguration.UAP_TOPIC_EXCHANGE, RabbitConfiguration.UAP_ANNUNCIATE_KEY, JSON.toJSONString(annunciateMessage));
+            rabbitMQMessageService.send(RabbitConfiguration.UAP_TOPIC_EXCHANGE, RabbitConfiguration.UAP_ANNUNCIATE_KEY, JSON.toJSONString(annunciateMessage));
             return BaseOutput.success("消息发送成功");
         } catch (Exception e) {
             return BaseOutput.failure("消息发送失败:"+e.getMessage());
--- uap/src/main/java/com/dili/uap/component/UserListener.java
+++ uap/src/main/java/com/dili/uap/component/UserListener.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.dili.ss.util.AESUtils;
 import com.dili.uap.manager.WsSessionManager;
 import com.dili.uap.sdk.domain.dto.AnnunciateMessage;
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
@@ -13,6 +14,8 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
 
 /**
  * 用户消费组件
@@ -32,11 +35,12 @@ public class UserListener {
      * @throws Exception
      */
     @RabbitListener(queues = "#{rabbitConfiguration.UAP_ADD_USER_QUEUE}")
-    public void addUser(Message message) throws Exception {
+    public void addUser(Channel channel, Message message) throws Exception {
         logger.info("收到消息: " + message);
-        String data = new String(message.getBody(), "UTF-8");
-        String json = AESUtils.decrypt(data, aesKey);
         try {
+            String data = new String(message.getBody(), "UTF-8");
+            String json = AESUtils.decrypt(data, aesKey);
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
             logger.info("消息解密: " + json);
         } catch (Exception e) {
             logger.error("转换User对象: {} 出错 {}", message, e);
@@ -49,14 +53,16 @@ public class UserListener {
      * @throws Exception
      */
     @RabbitListener(queues = "#{rabbitConfiguration.UAP_CHANGE_PASSWORD_QUEUE}")
-    public void changePassword(Message message) throws Exception {
+    public void changePassword(Channel channel, Message message) throws Exception {
         logger.info("收到消息: " + message);
-        String data = new String(message.getBody(), "UTF-8");
-        String json = AESUtils.decrypt(data, aesKey);
         try {
+            String data = new String(message.getBody(), "UTF-8");
+            String json = AESUtils.decrypt(data, aesKey);
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
             logger.info("消息解密: " + json);
         } catch (Exception e) {
             logger.error("转换User对象: {} 出错 {}", message, e);
+            handleException(channel, message);
         }
     }
 
@@ -66,17 +72,44 @@ public class UserListener {
      * @throws Exception
      */
     @RabbitListener(queues = "#{rabbitConfiguration.UAP_ANNUNCIATE_QUEUE}")
-    public void annunciate(Message message) throws Exception {
-        String data = new String(message.getBody(), "UTF-8");
+    public void annunciate(Channel channel, Message message) {
         try {
+            String data = new String(message.getBody(), "UTF-8");
             AnnunciateMessage annunciateMessage = JSON.parseObject(data, AnnunciateMessage.class);
             WebSocketSession webSocketSession = WsSessionManager.get(annunciateMessage.getTargetId());
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
             if(webSocketSession == null){
                 return;
             }
             webSocketSession.sendMessage(new TextMessage(data));
         } catch (Exception e) {
             logger.error("转换User对象: {} 出错 {}", message, e);
+            handleException(channel, message);
+        }
+    }
+
+    /**
+     * 处理消息接收后处理异常，扔回MQ
+     * @param channel
+     * @param message
+     */
+    public void handleException(Channel channel, Message message){
+        // redelivered = true, 表明该消息是重复处理消息
+        Boolean redelivered = message.getMessageProperties().getRedelivered();
+        try {
+            if (redelivered) {
+                /**
+                 * 1. 对于重复处理的队列消息做补偿机制处理
+                 * 2. 从队列中移除该消息，防止队列阻塞
+                 */
+                // 消息已重复处理失败, 扔掉消息
+                channel.basicReject(message.getMessageProperties().getDeliveryTag(), false);
+                logger.error("消息 {} 重新处理失败，扔掉消息", message);
+            } else {
+                channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);
+            }
+        } catch (IOException ex) {
+            logger.error("消息 {} 重放回队列失败 {}", message, ex);
         }
     }
 }
